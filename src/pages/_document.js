078162832ed7815a7b2e355d773503369d6cf627
Sodiq Sanusi <ademilddiq@gmail.com>
2022-07-14 19:19:04 +0100

    Modified the code gotten from Colby Fayock

---
+++ src/pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <meta name='description' content='An application that gives you the live location of an IP Address on a map!'/>
+          <meta name='author' content='Sodiq Sanusi'/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+          <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
